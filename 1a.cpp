e5c2b16133657d7d7a2c7c9773a0018a4ce402f6
Daviddor95 <gdavidexg@gmail.com>
2021-03-20 16:55:16 +0200

    new func

--- 1a.cpp
+++ 1a.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+void prt() {
+    std::cout << "prt" << std::endl;
+}
+
 int main() {
     std::cout << "hi" << std::endl;
     std::cout << "hi again" << std::endl;
     std::cout << "hello" << std::endl;
+    prt();
     return 0;
 }
